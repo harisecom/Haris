62ddbac4deac65fad3c3392fcc0fe0463a2720b1
Bhupendra <bhupendra1011@gmail.com>
2020-08-26 11:12:40 +0530

    setting up eslint config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-      <div>
-        Haris Ecome Application
-      </div>
-  );
+  return <div>Haris Ecome Application</div>;
 }
 
 export default App;
