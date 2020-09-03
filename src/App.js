03700875ab49f135612be21a44461c5b76d2bc17
Tai Jun Jie <tjunjie2000@gmail.com>
2020-09-03 14:53:11 +0800

    Used material UI instead of react-rating package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Footer from './Components/Footer/Footer.component';
+import RatingComponent from './Components/Rating/Rating';
 
 function App() {
   return (
     <div>
       Haris Ecome Applicatio
       <Footer></Footer>
+      <RatingComponent></RatingComponent>
     </div>
   );
 }
--- src/Components/Rating/Rating.jsx
+++ src/Components/Rating/Rating.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Rating.styles.css';
-import Rating from 'react-rating';
+import Rating from '@material-ui/lab/Rating'
 const RatingComponent = () => {
+    const [rating, setRating] = useState();
     return (
         <div className="container d-flex flex-column">
-        <span>Rating component, product will be right above</span>
-        <Rating
-            className="stars"
-            emptySymbol="fa fa-star fa-2x empty-star"
-            fullSymbol="fa fa-star fa-2x full-star"
-        />
-        <textarea className="form-control" placeholder="Ratings goes here"></textarea>
-        <button type="button" className="btn btn-primary align-self-end mt-3">Submit Review</button>
-
+            <span>Rating component, product will be right above</span>
+            <Rating
+                name="simple-controlled"
+                value={rating}
+                onChange={(event) => {
+                    setRating(event.target.value);
+                }}
+            />
+            <textarea className="form-control" placeholder="Ratings goes here"></textarea>
+            <button type="button" className="btn btn-primary align-self-end mt-3">Submit Review</button>
         </div>
 
     )
