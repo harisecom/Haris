4516009f7eea4204539ac62aa6db34129b041e43
Tai Jun Jie <tjunjie2000@gmail.com>
2020-09-07 18:11:46 +0800

    Fixed footer component styles

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import './App.css';
 import Footer from './Components/Footer/Footer.component';
 import ForgotPassword from './Pages/forgot-password/forgot-password';
 
-
 function App() {
   return (
     <div className="app-site">
       <div className="content">
         <ForgotPassword></ForgotPassword>
-        <Footer />
       </div>
+      <Footer />
     </div>
-   )
+  );
 }
 
 export default App;
