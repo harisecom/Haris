81412c8db97274d463ce988764d269e4e34b0445
Tai Jun Jie <tjunjie2000@gmail.com>
2020-09-13 17:10:40 +0800

    Added ratings section to product component page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import CartPage from './pages/cart';
 import ShopPage from './pages/shop';
 import NotFoundPage from './pages/not-found';
 import Footer from './Components/Footer/Footer.component';
+import ProductDetail from './Components/ProductDetails/ProductDetails';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/login" component={LoginPage} />
             <Route path="/cart" component={CartPage} />
             <Route path="/shop" component={ShopPage} />
+            <Route path="/product-detail" component={ProductDetail} />
             <Route path="*" component={NotFoundPage} />
           </Switch>
         </Router>
